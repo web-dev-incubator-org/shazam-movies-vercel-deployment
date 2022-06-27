1894bdcd911cbe41c447c5378b694a384c3bbca4
electronoob <github@electronoob.com>
2022-06-27 23:20:58 +0100

    trying to port over some of our ui, and a small bit of the express api server, to see if i can get it to deploy properly with our dependencies.

---
+++ api/base64file.js
@@ -0,0 +1,21 @@
+import express from "express"
+import bodyParser from "body-parser"
+import cors from "cors"
+import axios from "axios"
+
+const app = express();
+app.use(bodyParser.json({limit: "50mb"}))
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(cors({
+    origin: '*'
+}));
+
+app.use(bodyParser.json({limit: "50mb"}));
+app.use(bodyParser.urlencoded({limit: "50mb", extended: true}))
+
+app.post("/base64file", async (req, res, next) => {
+  res.setHeader('Content-Type', 'text/html');
+  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+  res.end(`received bytes:` + req.headers['content-length']);
+})
+
